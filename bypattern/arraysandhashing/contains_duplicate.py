ba889c912901849764fb56ae1f8dc5a33697fa76
bpupadhyaya <bpupadhyaya@gmail.com>
2026-02-08 11:24:39 -0800

    contains duplicate, problem definition.

---
+++ bypattern/arraysandhashing/contains_duplicate.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array,
+otherwise return false.
+Example 1:
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+Example2:
+Input: nums = [4,5,6]
+Output: false
+Tag: Blind 75, Blind 150
+"""
